74cdf1f106b5c98677a874642362bde36f2e5566
Luis Vega <vegaluisjose@gmail.com>
2021-07-20 20:26:38 +0000

    update

--- ultrascale/bram_rom/tcl/vivado.tcl
+++ ultrascale/bram_rom/tcl/vivado.tcl
@@ -11,8 +11,8 @@ set part_name "xczu3eg-sbva484-1-e"
 read_verilog -sv $verilog_file
 read_xdc -mode out_of_context $xdc_file
 synth_design -mode "out_of_context" -flatten_hierarchy "rebuilt" -top $top_name -part $part_name
-opt_design
-place_design -directive Default
+#opt_design
+#place_design -directive Default
 write_verilog -file $netlist_file
 #route_design -directive Default
 #report_timing -file $timing_file
--- ultrascale/lram_rom/v/main.v
+++ ultrascale/lram_rom/v/main.v
@@ -1,29 +1,29 @@
-module main(input clock, input reset, input [2:0] addr, output [7:0] y);
-
-  wire gnd;
-  wire vcc;
-  GND _gnd (
-      .G(gnd)
-  );
-  VCC _vcc (
-      .P(vcc)
-  );
-
-  // RAM64M8: 64-deep by 8-wide Multi Port LUT RAM (Mapped to eight LUT6s)
-  //          UltraScale
-  // Xilinx HDL Language Template, version 2020.2
-  (* LOC = "SLICE_X1Y1", BEL = "H6LUT" *)
-  RAM64M8 #(
-     .INIT_A(64'h0001110011001100), // Initial contents of A Port
-     .INIT_B(64'h0100111100001111), // Initial contents of B Port
-     .INIT_C(64'h0000111111110000), // Initial contents of C Port
-     .INIT_D(64'h0000111111111111), // Initial contents of D Port
-     .INIT_E(64'h0001110011001100), // Initial contents of E Port
-     .INIT_F(64'h0100111100001111), // Initial contents of F Port
-     .INIT_G(64'h0000111111110000), // Initial contents of G Port
-     .INIT_H(64'h0000111111111111), // Initial contents of H Port
-     .IS_WCLK_INVERTED(1'b0)        // Specifies active high/low WCLK
-  ) RAM64M8_inst (
+module main (
+    input wire clock,
+    input wire reset,
+    input wire [2:0] addr,
+    output wire [7:0] y
+);
+    wire gnd;
+    wire vcc;
+    GND _gnd (
+        .G(gnd)
+    );
+    VCC _vcc (
+        .P(vcc)
+    );
+    (*LOC = "SLICE_X0Y0", BEL = "H6LUT"*)
+    RAM64M8 # (
+        .INIT_A(64'h0001010001000100),
+        .INIT_B(64'h0100010100000101),
+        .INIT_C(64'h0000010101010000),
+        .INIT_D(64'h0000010101010101),
+        .INIT_E(64'h0001010001000100),
+        .INIT_F(64'h0100010100000101),
+        .INIT_G(64'h0000010101010000),
+        .INIT_H(64'h0000010101010101),
+        .IS_WCLK_INVERTED(1'b0)
+    ) __y (
         .ADDRA({addr[2], addr[1], addr[0], gnd, gnd, gnd}),
         .ADDRB({addr[2], addr[1], addr[0], gnd, gnd, gnd}),
         .ADDRC({addr[2], addr[1], addr[0], gnd, gnd, gnd}),
