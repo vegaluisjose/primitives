55df78ec731184fd7d91c1f5b6c1d732e3055b9d
Luis Vega <vegaluisjose@gmail.com>
2022-01-30 20:15:52 -0800

    add more parsing rules

--- lastmile/src/main.rs
+++ lastmile/src/main.rs
@@ -1,11 +1,24 @@
 use lastmile::parse;
 use std::fs::File;
 use std::io::Read;
+use std::path::Path;
+
+fn read_file<P: AsRef<Path>>(file: P) -> anyhow::Result<String> {
+    let mut f = File::open(file)?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+    Ok(contents)
+}
 
 fn main() -> anyhow::Result<()> {
-    let mut file = File::open("data/timing.rpt")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    println!("{}", parse::parse_latency(&contents)?);
+    let timing = read_file("data/timing.rpt")?;
+    let util = read_file("data/util.rpt")?;
+    println!("{}", parse::parse_latency(&timing)?);
+    println!("dsp:{:?}", parse::parse_dsp(&util)?);
+    println!("lut:{:?}", parse::parse_lut(&util)?);
+    println!("reg:{:?}", parse::parse_reg(&util)?);
+    println!("lram:{:?}", parse::parse_lram(&util)?);
+    println!("bram:{:?}", parse::parse_bram(&util)?);
+    println!("uram:{:?}", parse::parse_uram(&util)?);
     Ok(())
 }
--- lastmile/src/parse.rs
+++ lastmile/src/parse.rs
@@ -1,16 +1,101 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 
-pub fn parse_latency(text: &str) -> anyhow::Result<f32> {
+pub fn parse_latency(input: &str) -> anyhow::Result<f32> {
     lazy_static! {
         static ref RE: Regex =
-            Regex::new("Data Path Delay:[[:space:]]+([[:digit:]]+.[[:digit:]]+)ns").unwrap();
+            Regex::new(r"Data Path Delay:[[:space:]]+([[:digit:]]+.[[:digit:]]+)ns").unwrap();
     }
-    match RE.captures(text) {
+    match RE.captures(input) {
         Some(c) => match c[1].parse::<f32>() {
             Err(_) => Err(anyhow::anyhow!("could not convert latency to f32")),
             Ok(c) => Ok(c),
         },
         None => Err(anyhow::anyhow!("could not parse latency")),
+    }
+}
+
+// return used and available
+pub fn parse_dsp(input: &str) -> anyhow::Result<(u32, u32)> {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"\|[[:space:]]+DSPs[[:space:]]+\|[[:space:]]+([[:digit:]]+)[[:space:]]+\|[[:space:]]+[[:digit:]]+[[:space:]]+\|[[:space:]]+[[:digit:]]+[[:space:]]+\|[[:space:]]+([[:digit:]]+)[[:space:]]+\|").unwrap();
+    }
+    match RE.captures(input) {
+        Some(c) => match (c[1].parse::<u32>(), c[2].parse::<u32>()) {
+            (Ok(used), Ok(avail)) => Ok((used, avail)),
+            (_, _) => Err(anyhow::anyhow!("could not convert dsp used to u32")),
+        },
+        None => Err(anyhow::anyhow!("could not parse dsp used")),
+    }
+}
+
+pub fn parse_bram(input: &str) -> anyhow::Result<(u32, u32)> {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"\|[[:space:]]+Block RAM Tile[[:space:]]+\|[[:space:]]+([[:digit:]]+)[[:space:]]+\|[[:space:]]+[[:digit:]]+[[:space:]]+\|[[:space:]]+[[:digit:]]+[[:space:]]+\|[[:space:]]+([[:digit:]]+)[[:space:]]+\|").unwrap();
+    }
+    match RE.captures(input) {
+        Some(c) => match (c[1].parse::<u32>(), c[2].parse::<u32>()) {
+            (Ok(used), Ok(avail)) => Ok((used, avail)),
+            (_, _) => Err(anyhow::anyhow!("could not convert dsp used to u32")),
+        },
+        None => Err(anyhow::anyhow!("could not parse dsp used")),
+    }
+}
+
+pub fn parse_uram(input: &str) -> anyhow::Result<(u32, u32)> {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"\|[[:space:]]+URAM[[:space:]]+\|[[:space:]]+([[:digit:]]+)[[:space:]]+\|[[:space:]]+[[:digit:]]+[[:space:]]+\|[[:space:]]+[[:digit:]]+[[:space:]]+\|[[:space:]]+([[:digit:]]+)[[:space:]]+\|").unwrap();
+    }
+    match RE.captures(input) {
+        Some(c) => match (c[1].parse::<u32>(), c[2].parse::<u32>()) {
+            (Ok(used), Ok(avail)) => Ok((used, avail)),
+            (_, _) => Err(anyhow::anyhow!("could not convert dsp used to u32")),
+        },
+        None => Err(anyhow::anyhow!("could not parse dsp used")),
+    }
+}
+
+pub fn parse_lram(input: &str) -> anyhow::Result<(u32, u32)> {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"\|[[:space:]]+LUT as Memory[[:space:]]+\|[[:space:]]+([[:digit:]]+)[[:space:]]+\|[[:space:]]+[[:digit:]]+[[:space:]]+\|[[:space:]]+[[:digit:]]+[[:space:]]+\|[[:space:]]+([[:digit:]]+)[[:space:]]+\|").unwrap();
+    }
+    match RE.captures(input) {
+        Some(c) => match (c[1].parse::<u32>(), c[2].parse::<u32>()) {
+            (Ok(used), Ok(avail)) => Ok((used, avail)),
+            (_, _) => Err(anyhow::anyhow!("could not convert dsp used to u32")),
+        },
+        None => Err(anyhow::anyhow!("could not parse dsp used")),
+    }
+}
+
+pub fn parse_lut(input: &str) -> anyhow::Result<(u32, u32)> {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"\|[[:space:]]+LUT as Logic[[:space:]]+\|[[:space:]]+([[:digit:]]+)[[:space:]]+\|[[:space:]]+[[:digit:]]+[[:space:]]+\|[[:space:]]+[[:digit:]]+[[:space:]]+\|[[:space:]]+([[:digit:]]+)[[:space:]]+\|").unwrap();
+    }
+    match RE.captures(input) {
+        Some(c) => match (c[1].parse::<u32>(), c[2].parse::<u32>()) {
+            (Ok(used), Ok(avail)) => Ok((used, avail)),
+            (_, _) => Err(anyhow::anyhow!("could not convert dsp used to u32")),
+        },
+        None => Err(anyhow::anyhow!("could not parse dsp used")),
+    }
+}
+
+pub fn parse_reg(input: &str) -> anyhow::Result<(u32, u32)> {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"\|[[:space:]]+CLB Registers[[:space:]]+\|[[:space:]]+([[:digit:]]+)[[:space:]]+\|[[:space:]]+[[:digit:]]+[[:space:]]+\|[[:space:]]+[[:digit:]]+[[:space:]]+\|[[:space:]]+([[:digit:]]+)[[:space:]]+\|").unwrap();
+    }
+    match RE.captures(input) {
+        Some(c) => match (c[1].parse::<u32>(), c[2].parse::<u32>()) {
+            (Ok(used), Ok(avail)) => Ok((used, avail)),
+            (_, _) => Err(anyhow::anyhow!("could not convert dsp used to u32")),
+        },
+        None => Err(anyhow::anyhow!("could not parse dsp used")),
     }
 }
