f6662108efa12fcc4f16f867920828013d5c7e6a
Luis Vega <vegaluisjose@gmail.com>
2022-01-30 20:54:44 -0800

    remove main

--- lastmile/src/main.rs
+++
@@ -1,7 +0,0 @@
-use lastmile::profile::Profile;
-
-fn main() -> anyhow::Result<()> {
-    let profile = Profile::from_timing_and_utilization("data/timing.rpt", "data/util.rpt")?;
-    println!("{:#?}", profile);
-    Ok(())
-}
