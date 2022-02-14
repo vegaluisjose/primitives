32ead7e76bb15b6cece7366dd998fc267fb6e0fe
Luis Vega <vegaluisjose@gmail.com>
2022-02-14 01:21:55 +0000

    save data

---
+++ lastmile/aes_data/blk/constraint.xdc
@@ -0,0 +1,1 @@
+create_clock -period 8 -name clock -waveform {0.0 1} [get_ports clock]
---
+++ lastmile/aes_data/blk/vivado.tcl
@@ -0,0 +1,9 @@
+read_verilog -sv /home/vega/github/primitives/lastmile/aes/reticle_blk.v
+read_xdc -mode out_of_context constraint.xdc
+synth_design -mode "out_of_context" -flatten_hierarchy "rebuilt" -top toplevel -part "xck26-sfvc784-2LV-c"
+opt_design
+place_design -directive Default
+route_design -directive Default
+report_timing -file timing.rpt
+report_timing_summary -file timingsum.rpt
+report_utilization -file util.rpt
